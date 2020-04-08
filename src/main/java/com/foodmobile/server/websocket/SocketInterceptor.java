97630496e93cbd2a0f83544fe3e72aaef2a23c72
Jonathan Cooper <drowsysaturn@gmail.com>
2020-04-08 14:14:02 -0400

    Added id retrieval

--- src/main/java/com/foodmobile/server/websocket/SocketInterceptor.java
+++ src/main/java/com/foodmobile/server/websocket/SocketInterceptor.java
@@ -8,13 +8,27 @@ import org.springframework.web.socket.server.HandshakeInterceptor;
 
 import java.util.Map;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.foodmobile.server.util.jwt.JsonWebToken;
+
 public class SocketInterceptor implements HandshakeInterceptor {
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> attributes) throws Exception {
-        throw new Exception("Jonathan implement JWT here to make sure the connection is authenticated");
-        // TODO: put the appropriate user information in the attributes map, this becomes the session state once the websocket has finished the handshake process.
-        //return true;
+        try {
+            var tokenArr = serverHttpRequest.getHeaders().get("token");
+            if (tokenArr.size() > 0) {
+                var token = tokenArr.get(0);
+                var reader = JsonWebToken.verify(token);
+                var userId = reader.getInt("id");
+                attributes.put("id", userId);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (JWTVerificationException ex) {
+            return false;
+        }
     }
 
     @Override
